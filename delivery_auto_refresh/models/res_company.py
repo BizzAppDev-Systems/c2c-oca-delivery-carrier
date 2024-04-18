OCA/delivery-carrier

delivery-carrier: postpone new dependency to 17.0
This commit can be reverted in the migration to 17.0

delivery_auto_refresh/models/res_company.py
@@ -1,0 +1,14 @@
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    # Migration Note 17.0: move this to module sale_order_carrier_auto_assign
+    carrier_auto_assign_on_create = fields.Boolean(
+        "Set default shipping method automatically"
+    )
+    # End migration note

delivery_auto_refresh/models/res_config_settings.py
@@ -7,6 +7,13 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
+    # Migration Note 17.0: move this to module sale_order_carrier_auto_assign
+    carrier_auto_assign_on_create = fields.Boolean(
+        related="company_id.carrier_auto_assign_on_create",
+        readonly=False,
+    )
+    # End migration note
+
     auto_add_delivery_line = fields.Boolean(
         "Refresh shipping cost line automatically",
         config_parameter="delivery_auto_refresh.auto_add_delivery_line",

delivery_auto_refresh/models/sale_order.py
@@ -24,6 +24,56 @@ class SaleOrder(models.Model):
 
     # End migration note
 
+    # Migration Note 17.0: move this section to module sale_order_carrier_auto_assign
+    def _set_delivery_carrier(self, set_delivery_line=True):
+        for order in self:
+            delivery_wiz_action = order.action_open_delivery_wizard()
+            delivery_wiz_context = delivery_wiz_action.get("context", {})
+            if not delivery_wiz_context.get("default_carrier_id"):
+                continue
+            delivery_wiz = (
+                self.env[delivery_wiz_action.get("res_model")]
+                .with_context(**delivery_wiz_context)
+                .new({})
+            )
+
+            # Do not override carrier
+            if order.carrier_id:
+                delivery_wiz.carrier_id = order.carrier_id
+
+            # If the carrier isn't allowed, we won't default to it
+            if (
+                delivery_wiz.carrier_id
+                not in delivery_wiz.available_carrier_ids._origin
+            ):
+                return
+
+            if not set_delivery_line or order.is_all_service:
+                # Only set the carrier
+                if order.carrier_id != delivery_wiz.carrier_id:
+                    order.carrier_id = delivery_wiz.carrier_id
+            else:
+                delivery_wiz._get_shipment_rate()
+                delivery_wiz.button_confirm()
+
+    @api.onchange("partner_id", "partner_shipping_id")
+    def _add_delivery_carrier_on_partner_change(self):
+        partner = self.partner_shipping_id or self.partner_id
+        if not partner:
+            return
+        if self.company_id.carrier_auto_assign_on_create:
+            self._set_delivery_carrier(set_delivery_line=False)
+
+    def _is_auto_set_carrier_on_create(self):
+        self.ensure_one()
+        if self.state not in ("draft", "sent"):
+            return False
+        if self.company_id.carrier_auto_assign_on_create:
+            return True
+        return False
+
+    # End migration note
+
     def _get_param_auto_add_delivery_line(self):
         # When we have the context 'website_id' it means that we are doing the order from
         # e-commerce. So we don't want to add the delivery line automatically.
@@ -115,6 +165,10 @@ class SaleOrder(models.Model):
             .with_context(auto_refresh_delivery=False)
         )
         for order in orders:
+            # Migration Note 17.0: move this to module sale_order_carrier_auto_assign
+            if not order.carrier_id and order._is_auto_set_carrier_on_create():
+                order._set_delivery_carrier()
+            # End migration note
             order._auto_refresh_delivery()
         return orders
 

delivery_auto_refresh/tests/test_sale_order_carrier_auto_assign.py
@@ -1,0 +1,81 @@
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+# MIGRATION NOTE for 17.0: Move this to module sale_order_carrier_auto_assign
+
+from odoo.tests import Form, TransactionCase
+
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+
+
+class TestSaleOrderCarrierAutoAssignCommon(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env["base"].with_context(**DISABLED_MAIL_CONTEXT).env
+
+        cls.partner = cls.env.ref("base.res_partner_2")
+        cls.product_storable = cls.env.ref("product.product_product_9")
+        cls.delivery_local_delivery = cls.env.ref("delivery.delivery_local_delivery")
+        cls.delivery_local_delivery.fixed_price = 10
+
+
+class TestSaleOrderCarrierAutoAssignOnCreate(TestSaleOrderCarrierAutoAssignCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.settings = cls.env["res.config.settings"].create({})
+
+    def test_sale_order_carrier_auto_assign_onchange(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.carrier_auto_assign_on_create = True
+        self.settings.set_values()
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.partner
+        sale_order = sale_order_form.save()
+        self.assertEqual(sale_order.carrier_id, self.delivery_local_delivery)
+
+    def test_sale_order_carrier_auto_assign_create(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.carrier_auto_assign_on_create = True
+        self.settings.set_values()
+        sale_order = self.env["sale.order"].create({"partner_id": self.partner.id})
+        self.assertEqual(sale_order.carrier_id, self.delivery_local_delivery)
+
+    def test_sale_order_carrier_auto_assign_disabled(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.carrier_auto_assign_on_create = False
+        self.settings.set_values()
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.partner
+        sale_order = sale_order_form.save()
+        self.assertFalse(sale_order.carrier_id)
+
+    def test_sale_order_carrier_auto_assign_no_carrier(self):
+        self.partner.property_delivery_carrier_id = False
+        self.settings.carrier_auto_assign_on_create = True
+        self.settings.set_values()
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.partner
+        sale_order = sale_order_form.save()
+        self.assertFalse(sale_order.carrier_id)
+
+    def test_sale_order_carrier_auto_assign_carrier_already_set(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.carrier_auto_assign_on_create = True
+        carrier = self.env.ref("delivery.delivery_carrier")
+        sale_order = self.env["sale.order"].create(
+            {
+                "partner_id": self.partner.id,
+                "carrier_id": carrier.id,
+            }
+        )
+        self.assertEqual(sale_order.carrier_id, carrier)

BizzAppDev-Systems/c2c-oca-delivery-carrier

delivery_auto_refresh/models/res_company.py
@@ -1,0 +1,14 @@
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    # Migration Note 17.0: move this to module sale_order_carrier_auto_assign
+    sale_auto_assign_carrier_on_create = fields.Boolean(
+        "Set default shipping method automatically"
+    )
+    # End migration note

delivery_auto_refresh/models/res_config_settings.py
@@ -7,6 +7,13 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
+    # Migration Note 17.0: move this to module sale_order_carrier_auto_assign
+    sale_auto_assign_carrier_on_create = fields.Boolean(
+        related="company_id.sale_auto_assign_carrier_on_create",
+        readonly=False,
+    )
+    # End migration note
+
     auto_add_delivery_line = fields.Boolean(
         "Refresh shipping cost line automatically",
         config_parameter="delivery_auto_refresh.auto_add_delivery_line",

delivery_auto_refresh/models/sale_order.py
@@ -24,6 +24,54 @@ class SaleOrder(models.Model):
 
     # End migration note
 
+    # Migration Note 17.0: move this section to module sale_order_carrier_auto_assign
+    def _set_delivery_carrier(self, set_delivery_line=True):
+        for order in self:
+            delivery_wiz_action = order.action_open_delivery_wizard()
+            delivery_wiz_context = delivery_wiz_action.get("context", {})
+            if not delivery_wiz_context.get("default_carrier_id"):
+                continue
+            delivery_wiz = (
+                self.env[delivery_wiz_action.get("res_model")]
+                .with_context(**delivery_wiz_context)
+                .new({})
+            )
+
+            # Do not override carrier
+            if order.carrier_id:
+                delivery_wiz.carrier_id = order.carrier_id
+
+            # If the carrier isn't allowed, we won't default to it
+            if (
+                delivery_wiz.carrier_id
+                not in delivery_wiz.available_carrier_ids._origin
+            ):
+                return
+
+            if not set_delivery_line or order.is_all_service:
+                # Only set the carrier
+                if order.carrier_id != delivery_wiz.carrier_id:
+                    order.carrier_id = delivery_wiz.carrier_id
+            else:
+                delivery_wiz._get_shipment_rate()
+                delivery_wiz.button_confirm()
+
+    @api.onchange("partner_id", "partner_shipping_id")
+    def _add_delivery_carrier_on_partner_change(self):
+        partner = self.partner_shipping_id or self.partner_id
+        if not partner:
+            return
+        if self.company_id.sale_auto_assign_carrier_on_create:
+            self._set_delivery_carrier(set_delivery_line=False)
+
+    def _is_auto_set_carrier_on_create(self):
+        self.ensure_one()
+        if self.state not in ("draft", "sent"):
+            return False
+        return self.company_id.sale_auto_assign_carrier_on_create
+
+    # End migration note
+
     def _get_param_auto_add_delivery_line(self):
         # When we have the context 'website_id' it means that we are doing the order from
         # e-commerce. So we don't want to add the delivery line automatically.
@@ -113,6 +161,10 @@ class SaleOrder(models.Model):
             .with_context(auto_refresh_delivery=False)
         )
         for order in orders:
+            # Migration Note 17.0: move this to module sale_order_carrier_auto_assign
+            if not order.carrier_id and order._is_auto_set_carrier_on_create():
+                order._set_delivery_carrier()
+            # End migration note
             order._auto_refresh_delivery()
         return orders
 

delivery_auto_refresh/tests/test_sale_order_carrier_auto_assign.py
@@ -1,0 +1,81 @@
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+# MIGRATION NOTE for 17.0: Move this to module sale_order_carrier_auto_assign
+
+from odoo.tests import Form, TransactionCase
+
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+
+
+class TestSaleOrderCarrierAutoAssignCommon(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env["base"].with_context(**DISABLED_MAIL_CONTEXT).env
+
+        cls.partner = cls.env.ref("base.res_partner_2")
+        cls.product_storable = cls.env.ref("product.product_product_9")
+        cls.delivery_local_delivery = cls.env.ref("delivery.delivery_local_delivery")
+        cls.delivery_local_delivery.fixed_price = 10
+
+
+class TestSaleOrderCarrierAutoAssignOnCreate(TestSaleOrderCarrierAutoAssignCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.settings = cls.env["res.config.settings"].create({})
+
+    def test_sale_order_carrier_auto_assign_onchange(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.sale_auto_assign_carrier_on_create = True
+        self.settings.set_values()
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.partner
+        sale_order = sale_order_form.save()
+        self.assertEqual(sale_order.carrier_id, self.delivery_local_delivery)
+
+    def test_sale_order_carrier_auto_assign_create(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.sale_auto_assign_carrier_on_create = True
+        self.settings.set_values()
+        sale_order = self.env["sale.order"].create({"partner_id": self.partner.id})
+        self.assertEqual(sale_order.carrier_id, self.delivery_local_delivery)
+
+    def test_sale_order_carrier_auto_assign_disabled(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.sale_auto_assign_carrier_on_create = False
+        self.settings.set_values()
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.partner
+        sale_order = sale_order_form.save()
+        self.assertFalse(sale_order.carrier_id)
+
+    def test_sale_order_carrier_auto_assign_no_carrier(self):
+        self.partner.property_delivery_carrier_id = False
+        self.settings.sale_auto_assign_carrier_on_create = True
+        self.settings.set_values()
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.partner
+        sale_order = sale_order_form.save()
+        self.assertFalse(sale_order.carrier_id)
+
+    def test_sale_order_carrier_auto_assign_carrier_already_set(self):
+        self.assertEqual(
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
+        )
+        self.settings.sale_auto_assign_carrier_on_create = True
+        carrier = self.env.ref("delivery.delivery_carrier")
+        sale_order = self.env["sale.order"].create(
+            {
+                "partner_id": self.partner.id,
+                "carrier_id": carrier.id,
+            }
+        )
+        self.assertEqual(sale_order.carrier_id, carrier)