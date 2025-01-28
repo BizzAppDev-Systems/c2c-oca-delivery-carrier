4fd98f045341343a04623bacdbfeb5a3b572a40f
Ruchir Shukla <ruchir@bizzappdev.com>
2025-01-28 14:23:24 +0530

    [MIG] delivery_postlogistics: Migration to 18.0

--- delivery_postlogistics/models/delivery_carrier_template_option.py
+++ delivery_postlogistics/models/delivery_carrier_template_option.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2016 Camptocamp SA
+# Copyright 2013 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -15,18 +15,9 @@ POSTLOGISTICS_TYPES = [
 
 
 class DeliveryCarrierTemplateOption(models.Model):
-    """Available options for a carrier (partner)"""
+    _inherit = "delivery.carrier.template.option"
 
-    _name = "postlogistics.delivery.carrier.template.option"
-    _description = "Delivery carrier template option"
-
-    partner_id = fields.Many2one(comodel_name="res.partner", string="Carrier")
-    name = fields.Char(translate=True)
-    code = fields.Char()
-    description = fields.Char(
-        help="Allow to define a more complete description than in the name field.",
-    )
-    postlogistics_type = fields.Selection(
+    type = fields.Selection(
         selection=POSTLOGISTICS_TYPES,
         string="PostLogistics option type",
     )
