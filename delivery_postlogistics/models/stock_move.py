2d1efa39308d7342af85b6d093f2cac0ca4c9aec
Ruchir Shukla <ruchir@bizzappdev.com>
2025-01-28 14:23:24 +0530

    [MIG] delivery_postlogistics: Migration to 18.0

--- delivery_postlogistics/models/stock_move.py
+++ delivery_postlogistics/models/stock_move.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2016 Camptocamp SA
+# Copyright 2013 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -8,7 +8,7 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _get_new_picking_values(self):
-        vals = super(StockMove, self)._get_new_picking_values()
+        vals = super()._get_new_picking_values()
 
         order_commitment_date = (
             self.sale_line_id and self.sale_line_id.order_id.commitment_date
